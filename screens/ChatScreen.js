5030ddfe097fe02733d184e66b58509a3b11cdac
Ramalingam S <codeandgyan@gmail.com>
2023-08-15 20:39:03 +0530

    Introduced Chat button, Create Post(plus) button and fixed some styles

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,18 +1,18 @@
-import { Pressable, StyleSheet, Text, SafeAreaView } from "react-native";
+import { Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 
-const CreatePostScreen = ({ navigation }) => {
+const ChatScreen = ({ navigation }) => {
   return (
     <SafeAreaView>
       <Pressable onPress={() => navigation.navigate("Main")}>
         <Ionicons name="arrow-back-sharp" size={48} color="black" />
       </Pressable>
-      <Text>CreatePostScreen</Text>
+      <Text>ChatScreen</Text>
     </SafeAreaView>
   );
 };
 
-export default CreatePostScreen;
+export default ChatScreen;
 
 const styles = StyleSheet.create({});
